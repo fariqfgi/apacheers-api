6cea127b048cd317001252c603c5a71832709888
fariqfgi <fariq.fgi@gmail.com>
2022-03-02 21:48:18 +0700

    Add: Logout Route

--- routes/api.php
+++ routes/api.php
@@ -14,9 +14,8 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware(['auth:sanctum'])->group(function () {
+    Route::post('logout', [UserController::class, 'logout']);
 });
 
 Route::post('register', [UserController::class, 'register']);
