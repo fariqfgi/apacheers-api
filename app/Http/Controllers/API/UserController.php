ea53e882576724876073c90df9925436f98a6b02
fariqfgi <fariq.fgi@gmail.com>
2022-03-02 21:42:22 +0700

    Add: Logout Function

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -80,4 +80,10 @@ class UserController extends Controller
             ], 'Authentication Failed', 500);
         }
     }
+
+    public function logout(Request $request)
+    {
+        $token = $request->user()->currentAccessToken()->delete();
+        return ResponseFormatter::success($token, 'Token Revoked');
+    }
 }
